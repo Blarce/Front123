e9ac5111d87406d15a34e5a57ff9fcf6e132e367
denis.broslavsky <denis.broslavsky@simbirsoft.com>
2023-11-12 20:50:36 +0300

    auth refresh logic has been implemented

--- src/pages/api/index.tsx
+++ src/pages/api/index.tsx
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios, {AxiosError} from "axios";
+// import createAuthRefreshInterceptor from 'axios-auth-refresh'
 
 export const axiosInstance = axios.create({
     baseURL: 'http://localhost:8080',
@@ -8,6 +9,27 @@ export const axiosInstance = axios.create({
         'Content-Type': 'application/json',
     }
 });
+
+const refreshAuthLogic = (failedRequest: AxiosError) => {
+    return axiosInstance
+        .get('PATH TO REFRESH TOKEN ENDPOINT', {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('refreshToken')}`,
+            },
+        })
+        .then(({ data: { token, refreshToken } }) => {
+            localStorage.setItem('token', token)
+            localStorage.setItem('refreshToken', refreshToken)
+            if (failedRequest && failedRequest.response) {
+                failedRequest.response.config.headers['Authorization'] =
+                    'Bearer ' + token
+            }
+            return Promise.resolve()
+        })
+}
+
+// createAuthRefreshInterceptor(axiosInstance, refreshAuthLogic)
+
 axiosInstance.interceptors.request.use(function(config) {
     const token = localStorage.getItem('token');
     if(token) {
